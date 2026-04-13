a748ca619f9863beac7d4e00376a87fe2b913a14
saibabu2211 <saibabu.empcts@gmail.com>
2026-04-13 18:14:58 +0530

    Add climb_stairs.py with climbStairs function

---
+++ DP_QUESTIONS/climb_stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        a=1
+        b=2
+        if n==1:
+            return 1 
+        elif n==2:
+            return 2
+        else:
+            for i in range(2,n):
+                c=a+b 
+                a=b 
+                b=c
+        return b
